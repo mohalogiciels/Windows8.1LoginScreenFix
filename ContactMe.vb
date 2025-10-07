1e4bf7ed1e9c374054a418a6cfd55a81e0490a65
Moha Logiciels <mohalogiciels@hotmail.com>
2025-10-07 09:28:38 +0200

    update to version 2.2
    localisation added, minor bug fixes and code cleanup

---
+++ ContactMe.vb
@@ -0,0 +1,14 @@
+﻿Public Class ContactMe
+
+    Private Sub GitHubRepoLinkLabel_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles GitHubRepoLinkLabel.LinkClicked
+        Process.Start("https://www.github.com/mohalogiciels/Windows8.1LoginScreenFix")
+    End Sub
+
+    Private Sub EmailLinkLabel_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles EmailLinkLabel.LinkClicked
+        Process.Start("mailto:mohalogiciels@hotmail.com")
+    End Sub
+
+    Private Sub OKButton_Click(sender As Object, e As EventArgs) Handles OKButton.Click
+        Me.Dispose()
+    End Sub
+End Class
---
+++ InfoWindow.vb
@@ -0,0 +1,6 @@
+﻿Public Class InfoWindow
+
+    Private Sub OKButton_Click(sender As Object, e As EventArgs) Handles OKButton.Click
+        Me.Dispose()
+    End Sub
+End Class
